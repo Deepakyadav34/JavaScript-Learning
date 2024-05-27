005a6fa008f6fbf5dc8947a08fe14c017a5eb426
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-05-27 09:28:26 +0000

    convert

--- 01/02_conversion.js
+++ 01/02_conversion.js
@@ -18,3 +18,18 @@ console.log( typeof Numofnum1)
 console.log(Numofnum1)
 
 //here the value of Numofnum1 is NaN because the string contain both the value of number and string and can be converted into number but the value remains the NaN.
+
+
+// Similarly of we convert the striing to boolean then only the string havoing value will be true and if the string is empty then the boolean value will be false.
+
+let number11=''
+console.log(typeof number11)
+
+let booleanNumber=Boolean(number11)
+
+console.log(typeof booleanNumber)
+console.log(booleanNumber)
+
+//here the value is false because the string is empty and if there is any value present in string like deepak or anything then its boolean value will be true .
+//similarly we can convert the string to boolean and boolean to string.
+
