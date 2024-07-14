2ca53ee4df65a2502892a3f27b39ec53d0c61722
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-14 19:00:44 +0000

    start

---
+++ 03/1_Functions.js
@@ -0,0 +1,10 @@
+// Creating a function :
+
+function first(){
+    console.log("Hiii everyone my name is deepak")
+}
+// first() 
+// we can access the function using this method if the return statement is given in the function.
+
+// here we can use function in many situations :
+
