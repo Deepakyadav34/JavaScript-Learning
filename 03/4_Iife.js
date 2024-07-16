30ab05584329300f457daffe7a0b512f3a26bf87
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-16 17:36:57 +0000

    iife

--- 03/4_Iife.js
+++ 03/4_Iife.js
@@ -2,10 +2,10 @@
 // When there is so much code components and variables (global pollutants) then we will use the IIFE for faster 
 // execution of the code.
 
-// (function add(num1,num2){
-//     console.log(num1+ num2)
-//     }
-// // )(3,4);
+(function add(num1,num2){
+    console.log(num1+ num2)
+    }
+)(3,4);
 // here ; is must to stop the execution of  IIFE because IIFE knows about the initiation but dont know when and where to stop.
 
 // we can use arrow function in this too as :
