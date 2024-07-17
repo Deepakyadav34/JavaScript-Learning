412b6e442c2205016dfe9f0309488eee6c781712
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-17 18:47:58 +0000

    truthy

--- 04/truthy.js
+++ 04/truthy.js
@@ -35,3 +35,9 @@ val1 = null ?? 10 ?? 20
 
 console.log(val1);
 
+// Terniary Operator+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
