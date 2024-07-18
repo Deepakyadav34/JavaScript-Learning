5a49a14a97687ee4bed336a6813c333ee9a6d5d9
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-18 18:17:17 +0000

    IF

--- 04/one.js
+++ 04/one.js
@@ -2,41 +2,41 @@
 const isUserloggedIn = true
 const temperature = 41
 
-// if ( temperature === 40 ){
-//     console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
+if ( temperature === 40 ){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
 
-// console.log("Execute");
+console.log("Execute");
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+const score = 200
 
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
 
-// console.log(`User power: ${power}`);
+console.log(`User power: ${power}`);
 
 
-// const balance = 1000
+const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+if (balance > 500) console.log("test"),console.log("test2");
 
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
     
-// } else if (balance < 900) {
-//     console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 750");
     
-// } else {
-//     console.log("less than 1200");
+} else {
+    console.log("less than 1200");
 
-// }
+}
 
 const userLoggedIn = true
 const debitCard = true
