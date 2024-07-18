12791295a059798323a349d3511a9a4db4a40c1d
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-18 18:19:01 +0000

    truthy

--- 04/truthy.js
+++ 04/truthy.js
@@ -8,10 +8,10 @@ if (userEmail) {
 
 // falsy values
 
-false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 // truthy values
-"0", 'false', " ", [], {}, function(){}
+// "0", 'false', " ", [], {}, function(){}
 
 if (userEmail.length === 0) {
     console.log("Array is empty");
@@ -26,21 +26,21 @@ if (Object.keys(emptyObj).length === 0) {
 // Nullish Coalescing Operator (??): null undefined ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
 
 
 
-// console.log(val1);
+console.log(val1);
 
 // Terniary Operator+++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// condition ? true : false
+condition ? true : false
 
-// const iceTeaPrice = 100
-// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
 
 
 let x=1
