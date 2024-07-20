1f778ac4c96568a002fb484970e9c8aa00e4858a
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-20 10:26:45 +0000

    while and do while

--- 05/while&dowhile.js
+++ 05/while&dowhile.js
@@ -15,8 +15,23 @@ while (arr < myArray.length) {
 
 let score = 11
 
-do {
-    console.log(`Score is ${score}`);
-    score++
-} while (score <= 10);
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
 // in do while the action is taken first and then we check the condition thus it make the code to execute once.
+
+const a=3
+const b=new Number(3)
+const c='3'
+
+console.log(a==b)
+console.log(a===b)
+console.log(`type of a=${typeof a} and type of b =${typeof b}`)
+
+console.log(b==c)
+console.log(b===c)
+console.log(`type of b=${typeof b} and type of c =${typeof c}`)
+console.log(a==c)
+console.log(a===c)
+console.log(`type of a=${typeof a} and type of c =${typeof c}`)
