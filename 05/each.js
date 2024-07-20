3d5ae2608dd8c10555ed0bc10307249795558408
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-20 11:30:20 +0000

    loops

---
+++ 05/each.js
@@ -0,0 +1,61 @@
+// Here we will learn about for each loop in javascript.
+// Mainly foreach loops is used in arrays
+
+const arr=["deepak" , 'sheru','ritesh']
+
+// arr.forEach(function (val){
+//     console.log(val)
+// })
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// WE CAN USE ARROW FUNCTION AS WELL IN THIS 
+
+// arr.forEach((value)=>{
+//     console.log(value)
+// })
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+// we can also use some  external function to get the data as:
+
+function name(value){
+    console.log(value)
+}
+
+// arr.forEach(name) 
+// here we only have to provide the reference of the function and not fully execute the function.
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+// this foreach function can have more than one values to print in the function as:
+
+// arr.forEach( (value,index, arr) => {
+// // console.log(value,index,arr)
+// // })
+// so this will give the value of the value in array , index of the array and the total array arr.
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//  will it work for objects inside an array??
+
+const detail=[
+    {
+first_name:'Deepak',
+last_name:'Yadav'
+    },
+    {
+first_name:'Ayush',
+last_name:'Yadav'
+    },
+    {
+first_name:'Ritesh',
+last_name:'Singh'
+    }
+]
+
+detail.forEach( (value)=>{
+    // console.log(value)// here it will give all the values in the objects.
+
+    console.log(value.first_name)// gives the first_name and similarly we can access the last_name.
+}
+)
---
+++ 05/forINloops.js
@@ -0,0 +1,39 @@
+// we will use for in loops for the object iteration 
+
+const obj={
+    IN:'INDIA',
+    US:'UNITED STATE OF AMERICA',
+    SA:'SOUTH AFRICA',
+    WI:'WEST INDIES'
+};
+
+for(const keys in obj){
+    // console.log(keys)// for keys only
+    // console.log(`${keys} is for: ${obj[keys]}`)// this will give the key value pair
+}
+
+// will it work on arrays also??
+
+const arr=[1,2,3,4,5,6]
+for (const val in arr){
+    // console.log(val)// this will give the index value of the array only like 0,1,2,3...
+    
+    // so to get the value of the arrays we have to do this as:
+    // console.log(arr[val])
+
+}
+
+// let array = [1, 2, 3, 4, 5];
+// console.log(array.join(' '));// this is used to print the value of array in single line
+
+// will it work on MAP
+
+const map=new Map()
+map.set('IN','INDIA')
+map.set('US','UNITED STATE')
+map.set('AUS','AUSTRALIA')
+map.set('SA','SOUTH AFRICA')
+
+for(const [key] in map){
+    console.log(key) // this will not give any output so we can use for in loops in object iteration and in array
+}
