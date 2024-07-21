a77cdc19c1056926cf892f3d90dc9dc40b29524b
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-21 16:48:22 +0000

    chain

---
+++ 05/chaining.js
@@ -0,0 +1,16 @@
+// first we will learn about the map method as:
+
+// const nums=[1,2,3,4,5]
+
+// const value=nums.map( (val) => {return num+10})
+
+// console.log(value)
+// we can do the same with forEach method as:
+
+// nums.forEach( (val) => {console.log(val+10)})
+
+// Now the chaining method generally provides the details as:
+
+const numbers=[1,2,3,4,5,6,7,8,9]
+
+const nums= numbers.f
