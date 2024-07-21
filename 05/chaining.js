55f15bb138eb9f0f8bff1362cff22a8af0be6e96
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-21 17:05:55 +0000

    chaining

--- 05/chaining.js
+++ 05/chaining.js
@@ -11,6 +11,12 @@
 
 // Now the chaining method generally provides the details as:
 
-const numbers=[1,2,3,4,5,6,7,8,9]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const nums= numbers.f
+const newNums = numbers
+  .map((val) => val + 10)
+  .map((val) => val + 1)
+  .filter((val) => val >10)
+
+console.log(newNums);
+// so this is chaining method and we can add as many methods that we want in the chaining method.
