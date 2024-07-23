dee5090c31a41a450b14d4bf6477f29049cc520c
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-23 12:50:15 +0000

    /

--- 01_Basics/05_Numbers.js
+++ 01_Basics/05_Numbers.js
@@ -62,12 +62,12 @@ const max=6
 // let c=10
 // console.log(ab+c)
 
-let x=10
-if(true){
-    let x=20
-    // console.log(x)
-}
-// console.log(x)
+// let x=10
+// if(true){
+//     let x=20
+//     // console.log(x)
+// }
+// // console.log(x)
 
 let cc=1
 console.log(++cc + cc++)
