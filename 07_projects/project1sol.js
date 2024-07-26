83bce9c769734d96fcf5e159f12421a9143b1445
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-26 12:10:15 +0000

    project1

---
+++ 07_projects/project1sol.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  console.log(button);
+
+  // adding event listener
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    // if (e.target.id === 'grey') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'white') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'blue') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === 'yellow') {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+
+    //one more possible way of getting this is to use switch case
+
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'white':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'blue':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'yellow':
+        body.style.backgroundColor = e.target.id;
+        break;
+    }
+  });
+});
