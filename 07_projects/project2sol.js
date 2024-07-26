0617836f99f3e4163d5dc3aff4f9e4c80128d479
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-07-26 13:08:42 +0000

    solution 2

--- 07_projects/project2sol.js
+++ 07_projects/project2sol.js
@@ -7,12 +7,19 @@ form.addEventListener('submit', function (e) {
   const weight = parseInt(document.querySelector('#weight').value);
   const result = document.querySelector('#results');
 
-  if (height === 0 || isNaN(height) || height < 0) {
-    result.innerHTML = `please enter a valid height ${height}`;
-  } else if (weight === 0 || isNaN(weight) || weight < 0) {
-    result.innerHTML = `please enter a valid weight ${weight}`;
+  if (height === 0 || height < 0 || isNaN(height)) {
+    result.innerHTML = `Please Enter a valid height ${height} `;
+  } else if (weight === 0 || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `Please Enter a valid weight ${weight} `;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    result.innerHTML = bmi;
+
+    if (bmi < 18.6) {
+      result.innerHTML = `Your BMI is : ${bmi} and You are Under Weight`;
+    } else if (18.6 < bmi && bmi<24.9) {
+      result.innerHTML = `Your BMI is : ${bmi} and You are in Normal Range`;
+    } else {
+      result.innerHTML = ` Your BMI is: ${bmi} and You are Overweight `;
+    }
   }
 });
