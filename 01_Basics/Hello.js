307e8bfad494a1ed3bbc72fc8f297fdbd7379529
Deepak Yadav <114597498+Deepakyadav34@users.noreply.github.com>
2024-08-05 18:14:42 +0000

    Basics

--- 01_Basics/Hello.js
+++ 01_Basics/Hello.js
@@ -1,1 +1,9 @@
-console.log("Hello Deepak")
+// console.log("Hello Deepak")
+
+async function showTime() {
+    console.log('@@@@@@')
+    
+}
+const naam=  showTime()
+// console.log(typeof naam)//object
+// console.log('@@@@@@'===naam) // false 
